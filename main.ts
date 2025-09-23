018352d1c63252ff43b3d36f0f6a05382af9bd69
mercy-brice-kamilindi <mercy.brice.kamilindi@stu.ocsb.ca>
2025-09-23 12:41:28 -0400

    temperature in k

--- main.ts
+++ main.ts
@@ -5,7 +5,17 @@
  * This program gets the current temperature in k
 */
 
+// our varible for k
+let temperature : number
+
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
-// comment
+// temperature
+input.onButtonPressed(Button.A, function() {
+    temperature = input.temperature()
+    basic.showString('the temperature is')
+    basic.showNumber(temperature + 273)
+    basic.showString('k')
+    
+     })
